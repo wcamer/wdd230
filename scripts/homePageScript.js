24f75ee258726b78cd958343eefc855a5b8bd2ac
wcamer <xeus.xiii@hotmail.com>
2022-09-15 13:16:11 -0600

    beginning js

---
+++ scripts/homePageScript.js
@@ -0,0 +1,12 @@
+const copyWrite = '&copy' 
+let modifiedDate = document.lastModified;
+let currentYear = new Date().getFullYear()
+const author = 'William Cameron'
+const authorLocation = 'Utah, USA'
+
+console.log(currentYear);
+
+document.querySelector("#footerDate").innerHTML = copyWrite + currentYear; //`${copyYear} ${author}`;
+document.querySelector("#footerName").textContent = author;
+document.querySelector("#footerLocation").textContent = authorLocation;
+document.querySelector("#lastUpdated").textContent = `Last Updated: ${modifiedDate}`;
